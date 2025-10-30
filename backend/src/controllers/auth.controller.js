e4e06a15b76decc172dbfcd17376e04649eaebba
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2025-10-30 23:02:51 +0800

    feat: Implement signup API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,68 @@
-export const signup = (req, res) => {
-    res.send("signup route");
-}
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+// Signup route for users
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    // Check if all fields are provided, if not, return error message
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // Check if password is less than 6 and if so, return error message
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    // Find for existing user by email and if so, return error message
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    // Generate salt using bcrypt genSalt function with 10 rounds (by convention)
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash password using bcryptjs 123456 => sfddbfcshbdchsjchbcds
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user with hashed password
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // Generate JWT token here
+      generateToken(newUser.id, res);
+
+      // Await to save user
+      await newUser.save();
+
+      // Return 201 (created) status code
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      // Unable to create user
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 export const login = (req, res) => {
   res.send("login route");
-}
+};
 
 export const logout = (req, res) => {
   res.send("logout route");
-}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,19 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
+// Specify app requirements
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT;
 
+// Allows extraction of json data out of bosy
+app.use(express.json());
+
+// Specify routers
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
+// Set port for app to listen on
+app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
