6f3cbead2b439c11f9002b18324b28c5a2e361f0
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2025-10-30 17:58:14 +0800

    refactor: Formatted documents

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,17 @@
-import express from "express"
+import express from "express";
 
 const router = express.Router();
 
-router.get("/signup", (req,res) => {
-    res.send("Signup route");
+router.post("/signup", (req, res) => {
+  res.send("signup route");
 });
 
-router.get("/login", (req,res) => {
-    res.send("Login route");
+router.post("/login", (req, res) => {
+  res.send("login route");
 });
 
-router.get("/logout", (req,res) => {
-    res.send("logout route");
+router.post("/logout", (req, res) => {
+  res.send("logout route");
 });
 
 export default router;
