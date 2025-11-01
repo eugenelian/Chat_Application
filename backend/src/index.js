f5a0fd0a3c090d3b1361db9a71cde90fbd6e2bdb
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2025-11-02 01:57:16 +0800

    feat: Implemented building functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -11,6 +12,7 @@ import { app, server } from "./lib/socket.js";
 // Specify app requirements
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Allow app to use packages
 app.use(express.json());
@@ -20,6 +22,14 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // Specify routers
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// Setting up middleware for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Set port for app to listen on
 server.listen(PORT, () => {
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { axiosInstance } from "../lib/axios.js";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
   // Authenticated User Object
@@ -111,7 +112,7 @@ export const useAuthStore = create((set, get) => ({
       set({ onlineUsers: userIds });
     });
   },
-  
+
   disconnectSocket: () => {
     if (get().socket?.connected) get().socket.disconnect();
   },
